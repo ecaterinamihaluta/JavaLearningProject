cf98dd7efff55e1a157d54fe70cc9ef4af440b91
Katherine Mihaluta <katherine@compire.co>
2023-05-10 16:26:17 +0200

    Added the Variables and Comments Task

---
+++ src/main/java/org/ecaterina/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,48 @@
+package org.ecaterina.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+//         am declarat o serie de tipuri de variabile primitive cu min max
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+
+//Initializarea variabileleor declarate
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a' ;
+        lastCharFromAscii = 'Z';
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7e+308d;
+        upperLimitOfDoubleDataType = 1.7e+308;
+
+        System.out.println(firstCharFromAscii);
+
+
+    }
+
+
+}
